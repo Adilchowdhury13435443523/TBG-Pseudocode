2ec76eeeed80f06cd1a10952ef337c7fb734c2f1
Adilchowdhury13435443523 <146966749+Adilchowdhury13435443523@users.noreply.github.com>
2023-11-16 09:06:42 -0600

    Add files via upload

---
+++ TBG Pseudocode - Adil Chowdhury.py
@@ -0,0 +1,51 @@
+#TBG Pseudocode - Adil Chowdhury
+#Nov.16, 2023
+#Skeleton structure for our TBG that we will be building 
+
+#Introduce all the playable characters
+#Make the user pick one of the characters
+
+#Ask them which room they would like to go in
+#Conservatory, Library, Study, Ball Room
+
+#If Conservatory is picked
+    #Make sure to include peter hiding
+    #Ask if they want to questiong him
+
+    #If they say Yes
+        #Include information that peter has on the other characters
+        #After make the user walk back to the main lobby
+
+    #If they say no
+        #Make them walk back to the main lobby and ask if they want
+        #To pick up the knife
+
+#If Library is picked
+    #Make sure to include Bill and Johnny
+    #Ask if they want to question him
+
+    #If they say yes
+        #Include information that Bill and Johnny have on the other chracters
+        #After make the user walk back and find a gun
+
+    #If they say no
+        #Make them walk back to the main lobby and ask if they want
+        #to pick up the revolver
+
+#If Study is picked
+    #Make sure to include Dora
+    #Ask if they want to question her
+
+    #If they say yes
+        #Include information that Dora has on Johnny
+        #After make them walk back to the main lobby and ask if they want
+        #to follow the blood marks
+
+        #If they say Yes
+            #They will end up in the Library
+            #If they say no they will end up in the main lobby
+
+    #If they say no to questioning
+        #Make them walk back to the main lobby finding the blood marks
+
+
